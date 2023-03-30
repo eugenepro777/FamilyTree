ab4bccf0cd130378ff7b7d14571edd1f639f37d2
Eugene_Pro <proskuryakov.e@list.ru>
2023-03-30 23:55:33 +0300

    update Human

---
+++ FamilyConnect.java
@@ -0,0 +1,9 @@
+package FamilyTree;
+
+public enum FamilyConnect {
+    Father, Mother, Daughter,
+    Son, Brother, Sister,
+    Children, Husband, Wife,
+    Grandpa, Grandma, Grandson,
+    Granddaughter, Parents, Maidenname
+}
--- Human.java
+++ Human.java
@@ -1,39 +1,40 @@
 package FamilyTree;
 
+import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
-public class Human {
+public class Human implements Serializable {
     private String name;
     private String surname;
     private String dateBirth;
-    //не забываем про инкапсуляцию. Поля почти всегда private - сделал
 
+    //Не забываем про инкапсуляцию. Поля почти всегда private - сделал
     private Gender gender;
     private Human father;
     private Human mother;
+    private List<Human> childList;
 
-    List<Human> childList;
+    private Human lifeCompanion;
+    private String maidenName;
 
-//    public Human(String name, String surname, Gender gender, String dateBirth, Human father) {
-//        this.name = name;
-//        this.surname = surname;
-//        this.dateBirth = dateBirth;
-//        this.gender = gender;
-//        this.father = father;
-//        //this.childList = childList;
-//    }
+    private FamilyConnect connect;
+
+    private final String nameRegex = "^[\\p{L} .'-]+$";
 
     public Human(String name, String surname, Gender gender, String dateBirth) {
         this.name = name;
         this.surname = surname;
         this.dateBirth = dateBirth;
         this.gender = gender;
-        //this.father = father;
-        //this.childList = childList;
+
+        this.father = null;
+        this.mother = null;
+        this.lifeCompanion = null;
+        this.maidenName = "";
     }
 
     public Human(String name, String surname, Gender gender) {
@@ -48,6 +49,9 @@ public class Human {
         this.surname = surname;
     }
 
+    public Human() {
+
+    }
 
     public String getName() {
         return this.name;
@@ -78,24 +82,133 @@ public class Human {
     }
 
 
+//    public void setFather(Human father) {
+//        this.father = father;
+//    }
+
+
     public void setFather(Human father) {
-        this.father = father;
+        if (!this.hasConnect(FamilyConnect.Father)) {
+            if (!father.getChildList().contains(this)) {
+                father.getChildList().add(this);
+            }
+            this.father = father;
+        } else {
+            throw new IllegalArgumentException("Отец уже есть");
+        }
     }
+
+//    public void setMother(Human mother) {
+//        this.mother = mother;
+//    }
 
     public void setMother(Human mother) {
-        this.mother = mother;
+        if (!this.hasConnect(FamilyConnect.Mother)) {
+            if (!mother.getChildList().contains(this)) {
+                mother.getChildList().add(this);
+            }
+            this.mother = mother;
+        } else {
+            throw new IllegalArgumentException("Мать уже есть");
+        }
+    }
+
+
+// new methods
+
+    public int numChildren() {
+        return getChildList().size();
+    }
+
+    public void setChildList(List<Human> childList) {
+        this.childList = childList;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Human getLifeCompanion() {
+        return lifeCompanion;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public String getMaidenName() {
+        return maidenName;
+    }
+
+    public final void setNameRegex(String name) {
+        if (name.trim().matches(nameRegex)) {
+            this.name = name.trim();
+        } else {
+            throw new IllegalArgumentException("Неверно задано имя");
+        }
+    }
+
+    public final void setSurnameRegex(String surname) {
+        if (surname.trim().matches(nameRegex)) {
+            this.surname = surname.trim();
+        } else {
+            throw new IllegalArgumentException("Неверно задана фамилия");
+        }
+    }
+
+    public void setMaidenName(String maidenName) {
+        if (maidenName.trim().matches(nameRegex)) {
+            if (this.gender == Gender.Male) {
+                this.maidenName = maidenName.trim();
+            } else {
+                throw new IllegalArgumentException("Девичья фамилия для женщин");
+            }
+        } else if (maidenName.isEmpty()) {
+            this.maidenName = "";
+        } else {
+            throw new IllegalArgumentException("Неверно задана фамилия");
+        }
+
+    }
+
+    public boolean hasConnect(FamilyConnect connect) {
+        switch (connect) {
+            case Father -> {
+                return this.getFather() != null;
+            }
+            case Mother -> {
+                return this.getMother() != null;
+            }
+            case Husband, Wife -> {
+                return this.getLifeCompanion() != null;
+            }
+            case Parents -> {
+                return this.hasConnect(FamilyConnect.Father) || this.hasConnect(FamilyConnect.Mother);
+            }
+            case Maidenname -> {
+                return !this.getMaidenName().isEmpty();
+            }
+            case Children -> {
+                return !this.getChildList().isEmpty();
+            }
+            case Grandpa -> {
+                return this.father.getFather() != null;
+            }
+        }
+        return false;
     }
 
 
 
-//    public void addChild(String name, String surname) {
+
+
+    //    public void addChild(String name, String surname) {
 //        Human child = new Human(name, surname);
 //        child.setFather(this);
 //        this.childList.add(child);
 //    }
 
     //не хватает метода addChild  - добавил
-
     public void addChild(Human human) {
         childList = new ArrayList<>();
         this.childList.add(human);
@@ -124,9 +237,13 @@ public class Human {
                 "Пол: " + gender.name() + " " +
                 "Возраст: " + getAge(dateBirth) + " лет " + "\n" + "Отец: " + father;
     }
-
+// new methods
     public Human getMother() {
         return this.mother;
+    }
+
+    public Human getFather() {
+        return father;
     }
 
 //    public void addChild(Human child) {
